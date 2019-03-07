91d8ec2874274788506dbdacfa27045c042835d4
Moritz Fink <fink.moritz@gmail.com>
2019-03-07 16:50:22 +0100

    Add Exceptions example

---
+++ src/Exceptions.kt
@@ -0,0 +1,11 @@
+import java.lang.Exception
+
+fun main(args: Array<String>) {
+    try {
+        val x = 1/0
+    } catch (e:Exception) {
+        e.printStackTrace()
+    } finally {
+        println("Reached finally block")
+    }
+}
