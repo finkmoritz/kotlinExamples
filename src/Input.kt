ffab233622d2873059dc813aa5727fd3ca12b65a
Moritz Fink <fink.moritz@gmail.com>
2019-03-07 17:05:04 +0100

    Add Input & Scanner examples

---
+++ src/Input.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    print("Enter your text: ")
+    val inputString = readLine()
+    println("You entered: $inputString")
+}
---
+++ src/Scanner.kt
@@ -0,0 +1,8 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val myScanner = Scanner(System.`in`)
+    print("Enter your integer: ")
+    val x = myScanner.nextInt()
+    println("You entered the following integer: $x")
+}
